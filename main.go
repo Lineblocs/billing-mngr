5b450b113aaa139bd36f73e555959e8111cf960d
Nadir Hamid <matrix.nad@gmail.com>
2023-07-11 16:33:43 -0600

    ensure program stays running

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 import (
+	"time"
+	"fmt"
 	_ "github.com/Lineblocs/go-helpers"
 )
 
 func main() {
+
+	for ;; {
+		fmt.Println("Running billing commands")
+		time.Sleep(2 * time.Second)
+	}
 }
